235591d5b8d532ee38cc18cea4ca0ab779ab31ff
nibbler29 <planetexpressflightship@gmail.com>
2021-05-21 11:39:23 +0100

    asdwqe

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "ajax": "data/data.json",
-        "columns": [
-            { "data": "id" },
-            { "data": "topic" },
-            { "data": "type" },
-            { "data": "name" },
-            { "data": "status" },
-            { "data": "date_pc" },
-            { "data": "maturity" },
-            { "data": "legal" },
-            { "data": "published" }
-        ]
-    } );
-} );
+$(document).ready( function () {
+  
+    var table = $('#example').DataTable({
+                "ajax": "data/data.json",
+                "columns": [
+                    { "data": "id" },
+                    { "data": "topic" },
+                    { "data": "type" },
+                    { "data": "name" },
+                    { "data": "status" },
+                    { "data": "date_pc" },
+                    { "data": "maturity" },
+                    { "data": "legal" },
+                    { "data": "published" }
+                ],
+              initComplete: function () {
+              count = 0;
+              this.api().columns().every( function () {
+                  var title = this.header();
+                  //replace spaces with dashes
+                  title = $(title).html().replace(/[\W]/g, '-');
+                  var column = this;
+                  var select = $('<select id="' + title + '" class="select2" ></select>')
+                      .appendTo( $(column.footer()).empty() )
+                      .on( 'change', function () {
+                        //Get the "text" property from each selected data 
+                        //regex escape the value and store in array
+                        var data = $.map( $(this).select2('data'), function( value, key ) {
+                          return value.text ? '^' + $.fn.dataTable.util.escapeRegex(value.text) + '$' : null;
+                                   });
+                        
+                        //if no data selected use ""
+                        if (data.length === 0) {
+                          data = [""];
+                        }
+                        
+                        //join array into string with regex or (|)
+                        var val = data.join('|');
+                        
+                        //search for the option(s) selected
+                        column
+                              .search( val ? val : '', true, false )
+                              .draw();
+                      } );
+   
+                  column.data().unique().sort().each( function ( d, j ) {
+                      select.append( '<option value="'+d+'">'+d+'</option>' );
+                  } );
+                
+                //use column title as selector and placeholder
+                $('#' + title).select2({
+                  multiple: true,
+                  closeOnSelect: false,
+                  placeholder: "Select a " + title
+                });
+                
+                //initially clear select otherwise first option is selected
+                $('.select2').val(null).trigger('change');
+              } );
+          }
+    });
+  } );
