320f15670c988febba1e797bdfa17884d3e3c3af
nibbler29 <planetexpressflightship@gmail.com>
2021-05-21 11:31:39 +0100

    test

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    $('#example').DataTable();
+    $('#example').DataTable( {
+        "ajax": "data/data.json",
+        "columns": [
+            { "data": "id" },
+            { "data": "topic" },
+            { "data": "type" },
+            { "data": "name" },
+            { "data": "status" },
+            { "data": "date_pc" },
+            { "data": "maturity" },
+            { "data": "legal" },
+            { "data": "published" }
+        ]
+    } );
 } );
